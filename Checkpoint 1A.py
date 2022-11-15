34de779b4fafd9a52c1df8211e2c6e5190227649
Frokydafroakie <114589879+Frokydafroakie@users.noreply.github.com>
2022-11-15 16:29:01 +0400

    Count to 10
    Counts to 10

---
+++ Checkpoint 1A.py
@@ -0,0 +1,17 @@
+
+print("this program counts up to 10")
+
+#import time
+import time
+
+#count-controlled loop
+for Seconds in range(1,11):
+        #print command for output
+        print (Seconds)
+        #Wait between Commands
+        time.sleep(1)
+
+#this is to make a 1 second wait between the print command and the counting
+time.sleep(1)
+
+print("finish")
